543051c8d14d03abcf38c4a66694be87754d9848
crystalMinersForge <65672593+crystalMinersForge@users.noreply.github.com>
2020-07-22 02:34:15 +0100

    Add files via upload

---
+++ src/main/java/com/zundrel/currency/items/ItemBase.java
@@ -0,0 +1,9 @@
+package com.zundrel.currency.items;
+
+import net.minecraft.item.Item;
+
+public class ItemBase extends Item {
+    public ItemBase() {
+        super(new Item.Properties().group(com.zundrel.currency.currency.TAB));
+    }
+}
---
+++ src/main/java/com/zundrel/currency/util/RegistryHandler.java
@@ -0,0 +1,34 @@
+package com.zundrel.currency.util;
+
+import com.zundrel.currency.items.ItemBase;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+
+public class RegistryHandler {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, com.zundrel.currency.currency.MOD_ID);
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, com.zundrel.currency.currency.MOD_ID);
+
+    public static void init() {
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+
+    //Items
+    public static final RegistryObject<Item> TWENTY_DOLLAR_BILL = ITEMS.register("twenty_dollar_bill", ItemBase::new);
+    public static final RegistryObject<Item> TEN_DOLLAR_BILL = ITEMS.register("ten_dollar_bill", ItemBase::new);
+    public static final RegistryObject<Item> QUARTER = ITEMS.register("quarter", ItemBase::new);
+    public static final RegistryObject<Item> PENNY = ITEMS.register("penny", ItemBase::new);
+    public static final RegistryObject<Item> NICKEL = ITEMS.register("nickel", ItemBase::new);
+    public static final RegistryObject<Item> HUNDRED_DOLLAR_BILL = ITEMS.register("hundred_dollar_bill", ItemBase::new);
+    public static final RegistryObject<Item> FIVE_DOLLAR_BILL = ITEMS.register("five_dollar_bill", ItemBase::new);
+    public static final RegistryObject<Item> FIFTY_DOLLAR_BILL = ITEMS.register("fifty_dollar_bill", ItemBase::new);
+    public static final RegistryObject<Item> DIME = ITEMS.register("dime", ItemBase::new);
+    public static final RegistryObject<Item> DOLLAR_BILL = ITEMS.register("dollar_bill", ItemBase::new);
+
+    //Blocks
+}
